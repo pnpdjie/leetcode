0d0f42ef06a2b117f47392037103aa41f8634a29
85 <542574439@qq.com>
2020-10-25 13:52:29 +0800

    fib

---
+++ 图解算法数据结构/src/动态规划/斐波那契数列/Solution.java
@@ -0,0 +1,59 @@
+package 动态规划.斐波那契数列;
+
+/**
+ * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项。斐波那契数列的定义如下：
+ *
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+ *
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 2
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：n = 5
+ * 输出：5
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= n <= 100
+ * 注意：本题与主站 509 题相同：https://leetcode-cn.com/problems/fibonacci-number/
+ *
+ */
+public class Solution {
+//    public int fib(int n) {
+//        if (n==0 || n==1){
+//            return n;
+//        }
+//        return fib(n-1)+fib(n-2);
+//    }
+
+
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int count = 0;
+        int left = 0;
+        int right = 1;
+        for (int i = 2; i <= n; i++) {
+            count = left + right;
+            if (count >= 1000000008) {
+                count -= 1000000007;
+            }
+
+            left = right;
+            right = count;
+            System.out.println(i + " " + count);
+        }
+        return count;
+    }
+
+}
