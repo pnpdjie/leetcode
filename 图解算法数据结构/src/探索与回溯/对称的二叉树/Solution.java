b29ada754819228b4b80ef459510bf507c1e4cd7
85 <542574439@qq.com>
2020-11-15 23:42:30 +0800

    对称二叉树

---
+++ 图解算法数据结构/src/探索与回溯/对称的二叉树/Solution.java
@@ -0,0 +1,75 @@
+package 探索与回溯.对称的二叉树;
+
+/**
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ *  
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 节点个数 <= 1000
+ * <p>
+ * 注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/
+ */
+public class Solution {
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root==null){
+            return true;
+        }
+        return checkChildren(root.left, root.right);
+    }
+
+    private boolean checkChildren(TreeNode left, TreeNode right) {
+        if (left == null && right != null) {
+            return false;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left != null && right != null && left.val == right.val) {
+            return checkChildren(left.left, right.right) && checkChildren(left.right, right.left);
+        }
+        return false;
+    }
+}
